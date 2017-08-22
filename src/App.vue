1d0999a0b84f732b395c8183b920e1de7071a945
waylee <969603910@qq.com>
2017-08-22 18:28:09 +0800

    修改不能编译bug

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
+    <v-home></v-home>
+    <div class="header">
+       header
+    </div>
+    <div class="content">
+      content
+    </div>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import home from './components/home/home.vue';
+
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    'v-home':home
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
 }
 </style>
